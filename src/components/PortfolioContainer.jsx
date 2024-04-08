d20bca57905fc7a73691932a57fdf6522b1b5a78
Matthew <mthomaswicher@gmail.com>
2024-04-08 11:25:05 -0400

    remove lottie

--- src/components/PortfolioContainer.jsx
+++ src/components/PortfolioContainer.jsx
@@ -4,13 +4,12 @@ import Navbar from 'react-bootstrap/Navbar';
 import { NavDropdown } from 'react-bootstrap';
 import { Nav } from 'react-bootstrap';
 import matt from '../assets/matt.svg';
-import PropTypes from 'prop-types';
 
 const PortfolioContainer = (props) => {
   return (
     <Container fluid>
-      <Navbar>
-        <Container fluid>
+      <Navbar collapseOnSelect expand="small">
+        <Container>
           <Navbar.Brand>
             <img
               src={matt}
@@ -19,25 +18,12 @@ const PortfolioContainer = (props) => {
               height={16}
             />
           </Navbar.Brand>
-          <Navbar.Toggle aria-controls="responsive-navbar-nav" />
+          <Navbar.Toggle aria-controls="responsive-navbar-nav"></Navbar.Toggle>
           <Navbar.Collapse id="responsive-navbar-nav">
             <Nav className="mr-auto">
               <Nav.Link href="#home">Home</Nav.Link>
               <Nav.Link href="#features">Web3 Powered Remittances</Nav.Link>
               <Nav.Link href="#pricing">AI Personal Finance</Nav.Link>
-              <NavDropdown title="Dropdown" id="collasible-nav-dropdown">
-                <NavDropdown.Item href="#action/3.1">Action</NavDropdown.Item>
-                <NavDropdown.Item href="#action/3.2">
-                  Another action
-                </NavDropdown.Item>
-                <NavDropdown.Item href="#action/3.3">
-                  Something
-                </NavDropdown.Item>
-                <NavDropdown.Divider />
-                <NavDropdown.Item href="#action/3.4">
-                  Separated link
-                </NavDropdown.Item>
-              </NavDropdown>
             </Nav>
           </Navbar.Collapse>
         </Container>
